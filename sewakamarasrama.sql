4bb004239fe076fb81571fff6eb8bc5d7a041b3d
Harry Witriyono <49424961+HarryWitriyono@users.noreply.github.com>
2023-05-26 06:53:52 +0700

    Create sewakamarasrama.sql

---
+++ sewakamarasrama.sql
@@ -0,0 +1,3 @@
+create database sewakamarasrama;
+create table kamarasrama (nomorkamar int(2) not null auto_increment primary key, namakamar varchar(10) not null);
+create table penyewa (nik varchar(20) not null primary key, namapenyewa varchar(50) not null, nohp varchar(15) not null, tanggalmasuk date null, nomorkamar int(2) null, tanggalkeluar date null)
